e6e74f5967e3ca06b463fd6d77d508a18e860284
Xandeco Odyssey <alepbravo1702@gmail.com>
2024-11-05 02:57:49 -0300

    info panel inicio

---
+++ Assets/Scripts/UI/InfoPanel.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InfoPanel : MonoBehaviour
+{
+    [SerializeField] private RectTransform panelRect;
+    [SerializeField] private RectTransform titleRect;
+    [SerializeField] private TextMeshProUGUI infoPrefab;
+
+    [SerializeField] private float marginTop, marginBottom, marginLeft, marginRight;
+    [SerializeField] private float titleHeight, descriptionHeight, infoHeight;
+    [SerializeField] private float titleFontSize, descriptionFontSize, infoFontSize;
+    private string title;
+    private string description;
+    private string[] infos;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTitle(string title)
+    {
+        this.title = title;
+        
+    }
+
+    public void SetDescription(string description)
+    {
+        this.description = description;
+    }
+
+    public void SetInfos(string[] infos)
+    {
+        this.infos = infos;
+    }
+
+    public void UpdatePanel()
+    {
+
+    }
+}
