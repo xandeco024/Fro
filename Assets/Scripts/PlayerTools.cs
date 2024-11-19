fc8d10373a9ca5ccb8ebf19c9035476d4d4e4c98
Xandeco Odyssey <alepbravo1702@gmail.com>
2024-11-19 19:48:17 -0300

    algumas modificações

--- Assets/Scripts/PlayerTools.cs
+++ Assets/Scripts/PlayerTools.cs
@@ -10,9 +10,11 @@ public class PlayerTools : MonoBehaviour
     private TilemapManager tilemapManager;
     private Tile selectedTile;
 
+    [SerializeField] private float toolsRange;
+    private int currentToolIndex = 0;
+
     [Header("Destroy")]
     [SerializeField] private bool hasDestroyTool;
-    [SerializeField] private float destroyRange;
     [SerializeField] private float destroyDamage;
 
     [Header("Build")]
@@ -20,78 +22,128 @@ public class PlayerTools : MonoBehaviour
 
     [Header("Scanner")]
     [SerializeField] private bool hasScannerTool;
-    private float scanTimeCounter;
-    private float scanTime = .2f;
-    private bool scanning;
     private Tile lastScannedTile;
     [SerializeField] private GameObject infoPanelPrefab;
     private InfoPanel currentInfoPanel;
+
+    [Header("Watering")]
+    [SerializeField] private bool hasWateringTool;
+    [SerializeField] private int tankCapacity;
+    [SerializeField] private int tankLevel;
 
     void Start()
     {
         player = GetComponent<Player>();
         tilemapManager = FindObjectOfType<TilemapManager>();
+        tankLevel = tankCapacity;
     }
 
 
     void Update()
     {
-        if (hasDestroyTool) HandleDestroyTool(tilemapManager.GetSelectedTile());
-
-        if (hasScannerTool) {
-            //get player position relative to the canvas
-            if (selectedTile != null && lastScannedTile != selectedTile)
-            {
-                if (currentInfoPanel != null)
-                {
-                    Destroy(currentInfoPanel.gameObject);
-                }
-
-                lastScannedTile = selectedTile;
-                Vector2 playerPosition = Camera.main.WorldToScreenPoint(selectedTile.Coordinate);
-                currentInfoPanel = Instantiate(infoPanelPrefab, playerPosition, Quaternion.identity).GetComponent<InfoPanel>();
-                currentInfoPanel.GetComponent<InfoPanel>().UpdatePanel(
-                    selectedTile.Name,
-                    selectedTile.Temperature,
-                    selectedTile.Luminosity,
-                    selectedTile.LifeSupport,
-                    selectedTile.Wetness);
-                currentInfoPanel.transform.SetParent(GameObject.Find("HUD").transform);
-            }
-            // else if (GameObject.Find("Tile Info Panel(Clone)") != null)
-            // {
-            //     Destroy(GameObject.Find("Tile Info Panel(Clone)"));
-            // }
-        }
-
+        currentToolIndex = UpdateSelectedTool();
         selectedTile = tilemapManager.GetSelectedTile();
+
+        if (CanUseTool())
+        {
+            if (hasScannerTool) HandleScannerTool();
+
+            switch (currentToolIndex)
+            {
+                case 1:
+                    HandleDestroyTool();
+                    break;
+
+                case 2:
+                    //HandleBuildTool(selectedTile);
+                    break;
+
+                case 3:
+                    HandleWateringTool();
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 
-    public void SetDestroyTool(bool value)
+    private bool CanUseTool()
     {
-        hasDestroyTool = value;
+        return selectedTile != null && Vector3.Distance(player.transform.position, selectedTile.Coordinate) <= toolsRange;
     }
 
-    public void HandleDestroyTool(Tile tile)
+    private int UpdateSelectedTool()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1) && hasDestroyTool) return 1;
+        if (Input.GetKeyDown(KeyCode.Keypad2) && hasBuildTool) return 2;
+        if (Input.GetKeyDown(KeyCode.Keypad3) && hasWateringTool) return 3;
+
+        return currentToolIndex;
+    }
+
+    public void HandleDestroyTool()
     {
         if (Input.GetMouseButton(0))
         {
-            if (tile != null && Vector3.Distance(player.transform.position, tile.Coordinate) <= destroyRange)
+            selectedTile.Damage(destroyDamage * Time.deltaTime);
+            if (selectedTile.CurrentHealth <= 0)
             {
-                tile.Damage(destroyDamage * Time.deltaTime);
-                if (tile.CurrentHealth <= 0)
-                {
-                    tilemapManager.DestroyTile(tile.Coordinate);
-                }
+                tilemapManager.DestroyTile(selectedTile.Coordinate);
             }
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            if (tile != null)
+            selectedTile.Reset();
+        }
+    }
+
+    public void HandleScannerTool()
+    {
+        if (lastScannedTile != selectedTile)
+        {
+            if (currentInfoPanel != null)
             {
-                tile.Reset();
+                Destroy(currentInfoPanel.gameObject);
+            }
+
+            lastScannedTile = selectedTile;
+            Vector2 playerPosition = Camera.main.WorldToScreenPoint(selectedTile.Coordinate);
+            currentInfoPanel = Instantiate(infoPanelPrefab, playerPosition, Quaternion.identity).GetComponent<InfoPanel>();
+            currentInfoPanel.GetComponent<InfoPanel>().UpdatePanel(
+                selectedTile.Name,
+                selectedTile.Temperature,
+                selectedTile.Luminosity,
+                selectedTile.LifeSupport,
+                selectedTile.Wetness);
+            currentInfoPanel.transform.SetParent(GameObject.Find("HUD").transform);
+        }
+        else if (lastScannedTile == selectedTile && currentInfoPanel != null)
+        {
+            currentInfoPanel.UpdatePanel(
+                selectedTile.Name,
+                selectedTile.Temperature,
+                selectedTile.Luminosity,
+                selectedTile.LifeSupport,
+                selectedTile.Wetness);
+        }
+    }
+
+    private void HandleWateringTool()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (tankLevel > 0)
+            {
+                selectedTile.Water(1);
+                tankLevel--;
+            }
+            else
+            {
+                Debug.Log("Tank is empty!");
             }
         }
     }
+
 }
